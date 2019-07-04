95b158f39e445fc75e5ae615ee3b6255a5ac2564
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-05 10:23:50 +1200

    First version of `TagGenerator` with speech recognition being triggered from the `App`.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { Recorder } from './components/recorder';
+import React, { Component } from 'react';
+import { TagGenerator } from './components/tag-generator';
 
-function App() {
-  if (!navigator.mediaDevices) {
-    return (<div className="App">Sorry, this app is not supported by your browser or device.</div>);
+const App = class App extends Component {
+  state = {
+    tags: [],
+    recognizing: false,
   }
 
-  return (
-    <div className="App">
-      <h1>Recorder Test</h1>
-      <Recorder/>
-    </div>
-  );
+  toggleRecognition = () => {
+    const { recognizing } = this.state;
+    
+    this.setState({
+      recognizing: !recognizing,
+      tags: [],
+    });
+  }
+
+  render() {
+    if (!navigator.mediaDevices) {
+      return (<div className="App">Sorry, this app is not supported by your browser or device.</div>);
+    }
+
+    const { tags, recognizing } = this.state;
+  
+    return (
+      <div className="App">
+        <h1>Recorder Test</h1>
+        <TagGenerator
+          showResults={tags.length > 0}
+          startSpeechRecognition={recognizing}
+        />
+        <button onClick={this.toggleRecognition}>
+          {recognizing ?  'Listening to tag...' : 'Start tagging!'}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/tag-generator.tsx
+++ src/components/tag-generator.tsx
@@ -1,36 +1,122 @@
 import React, { Component, Fragment } from 'react';
+import { grammar as DEFAULT_GRAMMAR } from '../constants/grammar';
+import { start } from 'repl';
+
+enum TagGeneratorStatus {
+  INACTIVE = 0,
+  RECOGNIZING = 1,
+  STOPPED = 2,
+}
 
 interface ITagGeneratorProps {
-  showResults: boolean;
-  grammar: string; // @TODO: Check if there's a way to validate grammars.
+  startSpeechRecognition?: boolean;
+  speechGrammar?: string; // @TODO: Check if there's a way to validate grammars.
 }
 
 interface ITagGeneratorState {
+  speechRecognizer: any; // TODO: Get types for this.
+  tags: string[];
+  status: TagGeneratorStatus;
 }
 
 export const TagGenerator = class TagGenerator extends Component<ITagGeneratorProps, ITagGeneratorState> {
   constructor(props: ITagGeneratorProps) {
     super(props);
 
+    const { speechGrammar } = props;
+
+    // @ts-ignore -- For now...
+    const speechRecognitionConstructor = window.webkitSpeechRecognition || window.SpeechRecognition;
+    // @ts-ignore -- For now...
+    const speechGrammarListConstructor = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+
+    const speechRecognitionList = new speechGrammarListConstructor();
+    const speechRecognizer = new speechRecognitionConstructor();
+
+    speechRecognitionList.addFromString(speechGrammar || DEFAULT_GRAMMAR, 1);
+    speechRecognizer.grammars = speechRecognitionList;
+    speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
+    speechRecognizer.maxAlternatives = 1;
+
+    speechRecognizer.onresult = (e: any) => { this.updateTags(e) };
+    
     this.state = {
-      recognizer: null,
+      speechRecognizer,
+      tags: [],
+      status: TagGeneratorStatus.INACTIVE,
     }
   }
 
-  startRecognition = () => {
-
+  updateTags = (e: any) => {
+    console.log('Found a result: ', e);
   }
 
-  stopRecognition = () => {
+  renderInactiveStatus = () => {
+    return (
+      <Fragment>
+        <h2>Awaiting to be enabled...</h2>
+      </Fragment>
+    )
+  }
 
+  renderRecognizingStatus = () => {
+    return (
+      <Fragment>
+        <h2>Recording tags...</h2>
+      </Fragment>
+    )
+  }
+
+  renderStoppedStatus = () => {
+    const { tags } = this.state;
+
+    if (tags.length === 0) {
+      return (<h2>No tags found in speech.</h2>);
+    }
+
+    return (
+      <Fragment>
+        <h2>Tags found in speech</h2>
+        <p>{tags.map((tag: string, i: number) => `${i > 0 ? ',' : ''}${tag}`)}</p>
+      </Fragment>
+    )
+  }
+
+  componentDidUpdate() {
+    const { startSpeechRecognition } = this.props;
+    const { speechRecognizer, status } = this.state;
+
+    if (startSpeechRecognition && status !== TagGeneratorStatus.RECOGNIZING) {
+      speechRecognizer.start();
+
+      this.setState({
+        status: TagGeneratorStatus.RECOGNIZING,
+      });
+
+      return;
+    }
+
+    if (!startSpeechRecognition && status === TagGeneratorStatus.RECOGNIZING) {
+      speechRecognizer.stop();
+
+      this.setState({
+        status: TagGeneratorStatus.STOPPED,
+      });
+    }
   }
 
   render() {
-    return (
-      <Fragment>
-        <h2>This is the tag generator</h2>
-        <p>It receives a string and with the mic input</p>
-      </Fragment>
-    )
+    const { status } = this.state;
+
+    if (status === TagGeneratorStatus.INACTIVE) {
+      return this.renderInactiveStatus();
+    }
+
+    if (status === TagGeneratorStatus.RECOGNIZING) {
+      return this.renderRecognizingStatus();
+    }
+
+    return this.renderStoppedStatus();
   }
 }
