e5891c6128bca7706a5b85af82daa1624cbb2560
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-04 18:06:52 +1200

    Skeleton for `TagGenerator`.

--- src/components/tag-generator.tsx
+++ src/components/tag-generator.tsx
@@ -1,17 +1,35 @@
 import React, { Component, Fragment } from 'react';
 
 interface ITagGeneratorProps {
+  showResults: boolean;
+  grammar: string; // @TODO: Check if there's a way to validate grammars.
 }
 
 interface ITagGeneratorState {
 }
 
 export const TagGenerator = class TagGenerator extends Component<ITagGeneratorProps, ITagGeneratorState> {
+  constructor(props: ITagGeneratorProps) {
+    super(props);
+
+    this.state = {
+      recognizer: null,
+    }
+  }
+
+  startRecognition = () => {
+
+  }
+
+  stopRecognition = () => {
+
+  }
+
   render() {
     return (
       <Fragment>
         <h2>This is the tag generator</h2>
-        <p>It receives a Blob and return the words that it recognized based on a grammar</p>
+        <p>It receives a string and with the mic input</p>
       </Fragment>
     )
   }
