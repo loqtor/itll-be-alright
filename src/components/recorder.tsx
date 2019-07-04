c7182f2db7ff815c6c99fa3b2b2abc68a5eb1b79
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-04 16:52:32 +1200

    First version of `Recorder` that records and plays.

--- src/components/recorder.tsx
+++ src/components/recorder.tsx
@@ -4,22 +4,31 @@ interface IRecorderProps {}
 
 interface IRecorderState {
   isRecording: boolean;
-  hasRecorded: boolean;
+  audioAvailable: boolean;
   speechRecognition: any; // TODO: Get types for this.
+  recorder: any; // TODO: Get types for this.
+  audioUrl: string;
 }
 
+const DATA_AVAILABLE_INTERVAL = 500;
+
 export const Recorder = class Recorder extends Component<IRecorderProps, IRecorderState> {
+  isSettingMediaRecorder: boolean = false;
+  audioFragments: any[] = [];
+
   constructor(props: IRecorderProps) {
     super(props);
 
     // @ts-ignore -- For now...
-    const speechRecognitionConstructor = window.webkitSpeechRecognition || window.SpeechRecognition,
+    const speechRecognitionConstructor = window.webkitSpeechRecognition || window.SpeechRecognition;
 
     this.state = {
       isRecording: false,
-      hasRecorded: false,
-      speechRecognition: speechRecognitionConstructor();
-    }
+      audioAvailable: false,
+      speechRecognition: new speechRecognitionConstructor(),
+      recorder: null,
+      audioUrl: '',
+    };
   }
 
   toggleRecord = () => {
@@ -30,33 +39,84 @@ export const Recorder = class Recorder extends Component<IRecorderProps, IRecord
     } else {
       this.save();
     }
+  }
+
+  record = async () => {
+    const { recorder } = this.state;
+
+    if (!recorder) {
+      this.isSettingMediaRecorder = true;
+
+      return navigator
+        .mediaDevices
+        .getUserMedia({ audio: true })
+        .then(stream => {
+          // @ts-ignore -- Check what's up with the types
+          const mediaRecorder = new MediaRecorder(stream);
+
+          mediaRecorder.ondataavailable = (e: any) => {
+            console.log('At ondatavailable callback, e.data: ', e.data);
+            this.audioFragments.push(e.data);
+          }
+
+          mediaRecorder.onpause = () => {
+            /*
+             * This prevents the 416 error from happening.
+             * This forces the media recorder to create a new Blob when the recording is stopped.
+             */
+            mediaRecorder.requestData();
+          }
+
+          mediaRecorder.onresume = () => {
+            this.audioFragments = [];
+          }
+
+          this.setState({
+            recorder: mediaRecorder,
+            isRecording: true,
+          }, () => {
+            mediaRecorder.start(DATA_AVAILABLE_INTERVAL);
+          });
+        });
+    }
+
+    recorder.resume();
 
     this.setState({
-      isRecording: !isRecording,
+      isRecording: true,
     });
   }
 
-  record = () => {
-    console.log('Starting the recording.');
-  }
-
   save = () => {
-    console.log('Saving the recording.');
-  }
+    const { audioFragments } = this;
+    const { recorder } = this.state;
 
-  play = () => {
-    console.log('Playing what was recorded.');
+    recorder.pause();
+
+    console.log('After pause: ', audioFragments);
+
+    const audioBlob = new Blob(audioFragments, { 'type' : 'audio/ogg; codecs=opus' });
+    const audioUrl = URL.createObjectURL(audioBlob);
+
+    this.setState({
+      isRecording: false,
+      audioAvailable: true,
+      audioUrl,
+    });
   }
 
   render() {
-    const { hasRecorded, isRecording } = this.state;
+    const { audioAvailable, isRecording, audioUrl } = this.state;
     const buttonLabel = isRecording ? 'Stop' : 'Record';
+    const showAudio = audioAvailable && !isRecording;
 
     return (
       <Fragment>
         <h2>Press the button to record your message.</h2>
         <button onClick={this.toggleRecord}>{buttonLabel}</button>
-        {hasRecorded && <button onClick={this.play}>Play</button>}
+        {showAudio && (
+          <audio controls src={audioUrl}></audio>
+        )}
       </Fragment>
     )
   }
