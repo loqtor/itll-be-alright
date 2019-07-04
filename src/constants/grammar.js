3688db985548641ee57d6c726a63395c09679785
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-04 18:05:31 +1200

    Added grammar constants.

---
+++ src/constants/grammar.js
@@ -0,0 +1,2 @@
+const words = ['depressed', 'depression', 'oppresion', 'violence', 'violent', 'abuse', 'abused'];
+export const grammar = '#JSGF V1.0; grammar colors; public <color> = ' + feelings.join(' | ') + ' ;'
