3b3800c980725d4c97010a3407b6be1cb88c3310
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-04 14:57:50 +1200

    Added `Recorder` in `App` for testing. Added state toggle.

--- src/components/recorder.tsx
+++ src/components/recorder.tsx
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 interface IRecorderState {
   isRecording: boolean;
+  hasRecorded: boolean;
 }
 
 export const Recorder = class Recorder extends Component<{}, IRecorderState> {
+  state: IRecorderState = {
+    isRecording: false,
+    hasRecorded: false,
+  }
+
+  play = () => {
+    console.log('play to be implemented');
+  }
+
+  toggleRecord = () => {
+    const { isRecording } = this.state;
+
+    this.setState({
+      isRecording: !isRecording,
+    });
+  }
+
   render() {
+    const { hasRecorded, isRecording } = this.state;
+    const buttonLabel = isRecording ? 'Stop' : 'Record';
+
     return (
-      <p>This is the recorder component.</p>
+      <Fragment>
+        <h2>Press the button to record your message.</h2>
+        <button onClick={this.toggleRecord}>{buttonLabel}</button>
+        {hasRecorded && <button onClick={this.play}>Play</button>}
+      </Fragment>
     )
   }
 }
