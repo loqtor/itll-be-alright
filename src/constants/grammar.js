3fd6cfcd0a8b39829f4b4b795e9b8d9099842a7a
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-05 10:23:22 +1200

    Fixed wrong variable reference.

--- src/constants/grammar.js
+++ src/constants/grammar.js
@@ -1,2 +1,3 @@
-const words = ['depressed', 'depression', 'oppresion', 'violence', 'violent', 'abuse', 'abused'];
-export const grammar = '#JSGF V1.0; grammar colors; public <color> = ' + feelings.join(' | ') + ' ;'
+// const words = ['depressed', 'depression', 'oppresion', 'violence', 'violent', 'abuse', 'abused'];
+const words = ['things', 'stuff', 'another', 'night', 'test'];
+export const grammar = '#JSGF V1.0; grammar colors; public <color> = ' + words.join(' | ') + ' ;'
