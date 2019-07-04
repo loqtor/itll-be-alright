7bc04933b078eb1a2171d211ac9c28c174cbce22
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-07-04 16:52:09 +1200

    Added safeguard for `MediaRecorder`.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Recorder } from './components/recorder';
 
 function App() {
+  if (!navigator.mediaDevices) {
+    return (<div className="App">Sorry, this app is not supported by your browser or device.</div>);
+  }
+
   return (
     <div className="App">
       <h1>Recorder Test</h1>
